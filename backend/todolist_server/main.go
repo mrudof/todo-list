a5e535219b49c5278a0bb934c5a32499778d1d6a
Ian Meyer <ianmmeyer@gmail.com>
2017-12-19 10:38:57 -0500

    Add prometheus instrumentation/grpc logging

--- backend/todolist_server/main.go
+++ backend/todolist_server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
+	"net"
 	"net/http"
-	"os"
 
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	"github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/julienschmidt/httprouter"
 	"github.com/mrudof/todo-list/backend/todolist"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
@@ -25,16 +30,40 @@ func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-	logger := log.NewLogfmtLogger(os.Stdout)
-	logger = level.NewFilter(logger, level.AllowInfo())
-	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
 
-	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithInsecure()}
-	if err := todolist.RegisterTodoListHandlerFromEndpoint(ctx, mux, "localhost:50051", opts); err != nil {
-		level.Error(logger).Log("event", "server failed to start", err)
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("logger failed: %v", err)
 	}
 
-	level.Info(logger).Log("event", "server starting")
-	http.ListenAndServe(":8080", mux)
+	l, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		panic(err)
+	}
+
+	mux := runtime.NewServeMux()
+	s := grpc.NewServer(
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_prometheus.StreamServerInterceptor,
+			grpc_zap.StreamServerInterceptor(logger),
+		)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_prometheus.UnaryServerInterceptor,
+			grpc_zap.UnaryServerInterceptor(logger),
+		)),
+	)
+	grpc_prometheus.EnableHandlingTimeHistogram()
+	grpc_prometheus.Register(s)
+	go s.Serve(l)
+	todolist.RegisterTodoListServer(s, &server{})
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	if err := todolist.RegisterTodoListHandlerFromEndpoint(ctx, mux, "localhost:50051", opts); err != nil {
+		logger.Error(err.Error())
+	}
+
+	r := httprouter.New()
+	r.Handler("GET", "/metrics", promhttp.Handler())
+	r.Handler("GET", "/api/todo/list", mux)
+	logger.Info("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
 }
